1cfdd160f050b2b02b37656376842340c1f3e938
ABHIJITSINGH19 <132670625+ABHIJITSINGH19@users.noreply.github.com>
2025-12-05 23:13:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
+    port: 5173,
     proxy: {
       "/api": {
         target: process.env.VITE_API_URL || "http://localhost:4000",
